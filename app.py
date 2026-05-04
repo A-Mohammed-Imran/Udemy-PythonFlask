f4638d0ffa28aa31c942bf1197a03a48b2ed16a0
imran <mohammedimranat@gmail.com>
2026-05-04 15:54:11 +0530

    Add Flask routes, HTML templates, and static files for user interaction

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +9,8 @@ def index():
 def hello_name(name):
     return (f"Hello {name}!")
 
+# redirect and url_for example
+
 @app.route('/admin')
 def hello_admin():
     return "Hello Admin!"
@@ -16,8 +18,6 @@ def hello_admin():
 @app.route('/guest/<guest>')
 def hello_guest(guest):
     return f"Hello {guest} as Guest"
-    
-
 
 @app.route('/user/<name>')
 def hello_user(name):
@@ -25,6 +25,43 @@ def hello_user(name):
         return redirect(url_for('hello_admin'))
     else:
         return redirect(url_for('hello_guest', guest=name))
- 
+
+# HTTP Methods (GET, POST, etc.)
+
+@app.route('/welcome/<name>')
+def welcome(name):
+    return f"Welcome {name}!"
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user=request.form['nm']
+        return redirect(url_for('welcome', name=user))
+    else:
+        user=request.args.get('nm')
+        return redirect(url_for('welcome', name=user))
+    
+@app.route('/render')
+def render():
+    str = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Render Page</title>
+</head>
+<body>
+<p>This is a render html page.</p>
+</body>
+</html>"""
+    return str
+# we can also use the main html doc fromtemplates folder with the help of 'render_template' function
+
+# Static files (CSS, JavaScript, Images)
+
+@app.route('/static')
+def static_files():
+    return render_template('static.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,8 @@
+function sayHello() {
+    alert("Hello, World!");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("helloButton");
+    button.addEventListener("click", sayHello());
+});
